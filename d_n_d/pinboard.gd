ade652311e727e8c18af4f488dee26abf3ba5818
r-fedorov <rostislav.fedorov@h-its.org>
2026-01-31 22:13:50 +0100

    debug known inforamation

--- d_n_d/pinboard.gd
+++ d_n_d/pinboard.gd
@@ -4,8 +4,24 @@ var player
 var my_node
 var journal_open := false
 var manager
-var pinned_facts: Dictionary
 var state_d
+var pinned_facts: Dictionary = {
+	speaking_to = "peacock",
+	facts = {
+		fact_1 = {
+			known = false,
+			content = "This is a fact"
+		},
+		fact_2 = {
+			known = false,
+			content = "This is a fact"
+		},
+		fact_3 = {
+			known = false,
+			content = "This is a fact"
+		}
+	}
+	}
 
 
 # Called when the node enters the scene tree for the first time.
@@ -34,9 +50,10 @@ func _input(event):
 		
 		for fact_id in state_d.facts:
 			var fact = state_d.facts[fact_id]
+			var fact_known=pinned_facts.facts[fact_id]
 			
-			if pinned_facts[fact_id] == fact:
-				print_debug(fact_id, fact.known)
+			if pinned_facts.facts[fact_id] == fact:
+				print_debug(fact_known)
 				
 			
 			#if fact.known:
