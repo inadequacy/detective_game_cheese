0c1f0ce05f6e449510a232b67f131b40cf933a07
unknown <cole@mail.com>
2026-01-31 21:18:48 +0100

    fixed names of variables

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -1,15 +1,15 @@
 extends CharacterBody3D
 
-@export var speed = 6
-@export var rotation_speed = 1.5
+@export var speed = 9
+@export var rotation_speed = 2
 var journal = false
 
 func _physics_process(delta):
 	if journal == false:
 		var forward = -global_transform.basis.z
-		if Input.is_action_pressed("strafe_right"):
+		if Input.is_action_pressed("rotate_right"):
 			rotation.y -= rotation_speed * delta
-		if Input.is_action_pressed("strafe_left"):
+		if Input.is_action_pressed("rotate_left"):
 			rotation.y += rotation_speed * delta
 		if Input.is_action_pressed("move_forward"):
 			velocity = (forward * speed)
@@ -17,16 +17,9 @@ func _physics_process(delta):
 		if Input.is_action_pressed("move_backward"):
 			velocity = (-forward * speed / 1.7)
 			move_and_slide()
-			
-			
+
 	if Input.is_action_just_released("journal"):
-		
-		
 		if journal == false:
 			journal = true
-			
 		else:
 			journal=false
-		
-		
-		
