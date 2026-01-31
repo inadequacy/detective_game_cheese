3359531436bd25004b63e7c066dfede8aaeaecd0
unknown <cole@mail.com>
2026-01-31 11:00:41 +0100

    init, movement, game done, art severally lacking not my fault

---
+++ movement.gd
@@ -0,0 +1,17 @@
+extends CharacterBody3D
+
+@export var speed = 6
+@export var rotation_speed = 1.5
+
+func _physics_process(delta):
+	var forward = -global_transform.basis.z
+	if Input.is_action_pressed("strafe_right"):
+		rotation.y -= rotation_speed * delta
+	if Input.is_action_pressed("strafe_left"):
+		rotation.y += rotation_speed * delta
+	if Input.is_action_pressed("move_forward"):
+		velocity = (forward * speed)
+		move_and_slide()
+	if Input.is_action_pressed("move_backward"):
+		velocity = (-forward * speed / 1.7)
+		move_and_slide()
