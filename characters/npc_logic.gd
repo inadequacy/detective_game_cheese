a9719052e8098b2d0ed871a1eb2d349c3d88fabb
unknown <cole@mail.com>
2026-02-01 11:13:53 +0100

    move from a to b and big cheese in game

--- characters/npc_logic.gd
+++ characters/npc_logic.gd
@@ -1,21 +1,21 @@
 extends CharacterBody3D
 
 var manager
-@export var route_one = Node3D
-@export var route_two = Node3D
+@export var route_one : Array[Node3D]
+@export var route_two : Array[Node3D]
 @export var leave_time = 6
 var is_seen = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	manager = $"../Manager"
+	manager = $"../../Manager"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var target_position = route_one.global_transform.origin
+	# Path and when to go there go in here, need some kind of format for them
+	var target_position = route_one[0].global_transform.origin
 	position = position.move_toward(target_position, delta * 2)
 	
-	move_and_collide(velocity)
-	print(position)
+	move_and_slide()
 #	if manager.time == 5:
 #		pass
